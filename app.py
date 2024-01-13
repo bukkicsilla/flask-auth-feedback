d55afc14c9e9fe201012d2cfbc299876da764470
Csilla Bukki <csillabukki@Csillas-Air.hsd1.ca.comcast.net>
2024-01-12 21:36:34 -0800

    register and login routes for user added

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, redirect, session, flash
-from models import connect_db, db
+from models import connect_db, db, User
+from forms import RegisterForm, LoginForm
+from sqlalchemy.exc import IntegrityError
+
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///auth_feedback"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -9,6 +12,43 @@ app.config["SECRET_KEY"] = "123456"
 connect_db(app)
 
 @app.route("/")
-def home():
+def homepage():
+    #return render_template('home.html')
+    return redirect('/register')
+
+@app.route("/secret")
+def secret():
     return render_template('home.html')
 
+@app.route("/register", methods=['GET', 'POST'])
+def register_user():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        new_user = User.register(username, password, email, first_name, last_name)
+        db.session.add(new_user)
+        db.session.commit()
+        #session['user_id'] = new_user.id
+        flash('Welcome! Successfully Created Your Account!', "success")
+        return redirect('/home')
+    return render_template('register.html', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login_user():
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.authenticate(username, password)
+        if user:
+            flash(f"Welcome Back, {user.username}!", "primary")
+            #session['user_id'] = user.id
+            return redirect('/secret')
+        else:
+            form.username.errors = ['Invalid username or']
+            form.password.errors = ['Invalid password']
+    return render_template('login.html', form=form)
--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
+bcrypt = Bcrypt()
 
 def connect_db(app):
     """Connect to database."""
@@ -15,3 +17,21 @@ class User(db.Model):
     email = db.Column(db.String(50), nullable=False, unique=True)
     first_name = db.Column(db.String(30), nullable=False)
     last_name = db.Column(db.String(30), nullable=False)
+
+    @classmethod
+    def register(cls, username, password, email, first_name, last_name):
+        """Register user with username, hashed password, email, first_name, last_name and return user."""
+        hashed = bcrypt.generate_password_hash(password)
+        hashed_utf8 = hashed.decode("utf8")
+        return cls(username=username, password=hashed_utf8, email=email, first_name=first_name, last_name=last_name)
+    
+    @classmethod
+    def authenticate(cls, username, password):
+        """Validate that user exists and password is correct.
+        Return user if valid, else return False.
+        """
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            return user
+        else:
+            return False
